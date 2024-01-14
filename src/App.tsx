7baa0a2f46d4d321281a47054e6d318987fc0eaa
fabian0270 <fabianolsson28@gmail.com>
2024-01-15 00:54:44 +0100

    Finished test

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import  MyFirstComponent  from './MyFirstComponent'
+function App() {
+
+  return (
+    <>
+      <MyFirstComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/MyFirstComponent.tsx
@@ -0,0 +1,37 @@
+import "./MyFirstComponent.css";
+import { useState } from 'react';
+
+function App(): JSX.Element {
+  const [number1, setNumber1] = useState<number | undefined>();
+  const [number2, setNumber2] = useState<number | undefined>();
+  const [total, setTotal] = useState<number>(0);
+
+  function calculateTotal(): void {
+    if (number1 !== undefined && number2 !== undefined) {
+      setTotal(number1 + number2);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Adding Two Numbers</h2>
+      <input
+        placeholder="First Number"
+        type="number"
+        value={number1 || ''}
+        onChange={(e) => setNumber1(+e.target.value)}
+      />
+      <input
+        placeholder="Second Number"
+        type="number"
+        value={number2 || ''}
+        onChange={(e) => setNumber2(+e.target.value)}
+      />
+
+      <button onClick={calculateTotal}>Add Two Numbers</button>
+      <p>Total: {total || ''}</p>
+    </div>
+  );
+}
+
+export default App
